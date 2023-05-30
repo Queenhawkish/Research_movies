15127023a1fb070fc42f163ae118222e13c96c91
Aglaé <aglae.michot@gmail.com>
2023-05-30 11:27:48 +0200

    présentation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,29 @@
 let button_search = document.getElementById("button_search");
 
+const options = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzNGUzN2Y1YTU4ZTQ3ZGIwMGY4NTkyODU3OWY5MDBmOCIsInN1YiI6IjY0NmUxNjEzMzNhMzc2MDE1OGRjMDRhZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.10TTdpPPusGwjBn81duAdGN3P84qd250flrJJOeCyEs'
+    }
+};
+
 button_search.addEventListener("click", () => {
     let search = document.getElementById("search");
-
-    const options = {
-        method: 'GET',
-        headers: {
-            accept: 'application/json',
-            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzNGUzN2Y1YTU4ZTQ3ZGIwMGY4NTkyODU3OWY5MDBmOCIsInN1YiI6IjY0NmUxNjEzMzNhMzc2MDE1OGRjMDRhZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.10TTdpPPusGwjBn81duAdGN3P84qd250flrJJOeCyEs'
-        }
-    };
-
 
     fetch(`https://api.themoviedb.org/3/search/movie?query=${search.value}&include_adult=false&language=fr-FR`, options)
         .then(response => response.json())
         .then(movies => {
+            console.log(movies.results)
             movies.results.forEach(movies => {
-                if(movies.poster_path == null) {
+                if (movies.poster_path == null) {
                     contain.innerHTML += `
                     <div class="card bg-secondary card2" style="width: 18rem;">
                         <div class="empty_img">
                             <div class="text_empty_img">No poster</div>
                         </div>
                         <div class="card-body">
-                            <button type="button" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                            <button type="button" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#id${movies.id}">
                                 ${movies.original_title}
                             </button>
                             <p class="card-text">${movies.overview}</p>
@@ -32,7 +32,7 @@ button_search.addEventListener("click", () => {
 
 
                     <!-- Modal -->
-                    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal fade" id="id${movies.id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                         <div class="modal-dialog modal-dialog-centered">
                             <div class="card" style="width: 18rem;">
                                 <img src="..." class="card-img-top" alt="...">
@@ -46,8 +46,8 @@ button_search.addEventListener("click", () => {
                         </div>
                     </div>
         `
-                        } else{
-                            contain.innerHTML += `
+                } else {
+                    contain.innerHTML += `
                     <div class="card bg-secondary card2" style="width: 18rem;">
                         <img src="https://image.tmdb.org/t/p/w500/${movies.poster_path}" class="card-img-top" alt="Affiche">
                         <div class="card-body">
@@ -74,12 +74,81 @@ button_search.addEventListener("click", () => {
                         </div>
                     </div>
         `
-                        }
-                        })
+                }
+            })
 
         })
-        // .catch(err => console.error(err));
+        .catch(err => console.error(err));
 })
 
-fetch(`https://api.themoviedb.org/3/search/movie?query=${search.value}&include_adult=false&language=fr-FR`, options)
 
+fetch('https://api.themoviedb.org/3/movie/now_playing?language=fr-FR&page=1', options)
+    .then(response => response.json())
+    .then(latest =>{
+        latest.results.forEach(latest => {
+            if (latest.poster_path == null) {
+            contain.innerHTML += `
+            <div class="card bg-secondary card2" style="width: 18rem;">
+                <div class="empty_img">
+                    <div class="text_empty_img">No poster</div>
+                </div>
+                <div class="card-body">
+                    <h1>
+                        ${latest.original_title}
+                    </h1>
+                    <button type="button" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#id${latest.id}">
+                        + d'info
+                    </button>
+                </div>
+            </div>
+
+
+            <!-- Modal -->
+            <div class="modal fade" id="id${latest.id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered">
+                    <div class="card" style="width: 18rem;">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                of the card's content.</p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+`
+        } else {
+            contain.innerHTML += `
+            <div class="card bg-secondary card2" style="width: 18rem;">
+                <img src="https://image.tmdb.org/t/p/w200/${latest.poster_path}" class="card-img-top" alt="Affiche">
+                <div class="card-body">
+                <h1>
+                    ${latest.original_title}
+                </h1>
+                <button type="button" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#id${latest.id}">
+                    + d'info
+                </button>
+                </div>
+            </div>
+
+
+            <!-- Modal -->
+            <div class="modal fade" id="id${latest.id}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered">
+                    <div class="card" style="width: 18rem;">
+                        <img src="https://image.tmdb.org/t/p/w300/${latest.poster_path}" class="card-img-top" alt="Affiche">
+                        <div class="card-body">
+                            <h5 class="card-title">${latest.original_title}</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                of the card's content.</p>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+`}
+        })
+        
+    })
+    .catch(err => console.error(err));
